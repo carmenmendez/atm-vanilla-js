d9a76fdb1b9a4b443bae525e648d67ead6bc429e
carmenmendez <carmenmendez.github.io>
2023-01-05 18:45:10 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+import './style.css'
+
+var cuentas = [
+  { nombre: "Pamela", saldo: 200 },
+  { nombre: "Jay", saldo: 290 },
+  { nombre: "Diego", saldo: 67 }
+];
+
+// click
+cuentas[1].saldo = cuentas[1].saldo - 100
